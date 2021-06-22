c2d38a6a8ca95c0b5c36fba5d831b5f20bcf1df8
Samet UCA <sametuca@hotmail.com>
2021-06-22 16:27:04 +0300

    added base controller and config

---
+++ CoreArchitecture.API/Controllers/Base/ExceptionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CoreArchitecture.API.Controllers.Base
+{
+    public class ExceptionController : BaseController
+    {
+        private readonly ILogger<ExceptionController> _logger;
+
+        public ExceptionController(ILogger<ExceptionController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new Exception("Test exception");
+        }
+    }
+}
