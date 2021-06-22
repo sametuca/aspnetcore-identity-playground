d96852cec0a9b7e42dcf72a64cd31182bea1d899
Samet UCA <sametuca@hotmail.com>
2021-06-22 16:27:21 +0300

    add book controller

---
+++ CoreArchitecture.API/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreArchitecture.API.Controllers.Base;
+
+namespace CoreArchitecture.API.Controllers
+{
+    public class BookController : BaseController
+    {
+     
+    }
+
+}
